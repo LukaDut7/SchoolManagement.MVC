b1d043406d0f9d679e1e7b29058d8a3611f245b5
Luka <dutinaluka774@gmail.com>
2024-11-04 17:41:53 +0900

    Finished customizing Views and Elements

--- Data/Class.cs
+++ Data/Class.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 
 namespace SchoolManagement.MVC.Data;
 
-public partial class Student
+public partial class Class
 {
     public int Id { get; set; }
 
-    public string FirstName { get; set; } = null!;
+    public int? LecturerId { get; set; }
 
-    public string LastName { get; set; } = null!;
+    public int? CourseId { get; set; }
 
-    public DateOnly? DateOfBirth { get; set; }
+    public virtual Course? Course { get; set; }
 
     public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+
+    public virtual Lecture? Lecturer { get; set; }
 }
--- Data/SchoolManagementDbContext.cs
+++ Data/SchoolManagementDbContext.cs
@@ -6,16 +6,16 @@ namespace SchoolManagement.MVC.Data;
 
 public partial class SchoolManagementDbContext : DbContext
 {
-    public SchoolManagementDbContext()
-    {
-    }
-
     public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
         : base(options)
     {
     }
 
+    public virtual DbSet<Class> Classes { get; set; }
+
     public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
 
     public virtual DbSet<Lecture> Lectures { get; set; }
 
@@ -23,19 +23,47 @@ public partial class SchoolManagementDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Class>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Classes__3214EC0728101DF7");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK__Classes__CourseI__3F466844");
+
+            entity.HasOne(d => d.Lecturer).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.LecturerId)
+                .HasConstraintName("FK__Classes__Lecture__3E52440B");
+        });
+
         modelBuilder.Entity<Course>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC074AD98C59");
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC072E95FD12");
 
-            entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA72BBE861C").IsUnique();
+            entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA7CCD676C0").IsUnique();
 
             entity.Property(e => e.Code).HasMaxLength(5);
             entity.Property(e => e.Name).HasMaxLength(50);
         });
 
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Enrollme__3214EC07FE4C3A88");
+
+            entity.Property(e => e.Grade).HasMaxLength(2);
+
+            entity.HasOne(d => d.Class).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.ClassId)
+                .HasConstraintName("FK__Enrollmen__Class__4316F928");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .HasConstraintName("FK__Enrollmen__Stude__4222D4EF");
+        });
+
         modelBuilder.Entity<Lecture>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC0765FA4E50");
+            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC076D589306");
 
             entity.Property(e => e.FirstName).HasMaxLength(50);
             entity.Property(e => e.LastName).HasMaxLength(50);
@@ -43,7 +71,7 @@ public partial class SchoolManagementDbContext : DbContext
 
         modelBuilder.Entity<Student>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07288DC93B");
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC075F610DA0");
 
             entity.Property(e => e.FirstName).HasMaxLength(50);
             entity.Property(e => e.LastName).HasMaxLength(50);
